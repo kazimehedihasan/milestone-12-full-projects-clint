d634ebf86235890cbbbc74603a16fdfc63b14d5a
kazimehedihasan <kazimehedihasan458@.com>
2023-11-22 12:46:59 +0600

    first commit

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,46 +1,107 @@
+import { useRef, useState } from "react";
+import { useEffect } from "react";
+import {
+  loadCaptchaEnginge,
+  LoadCanvasTemplate,
+  LoadCanvasTemplateNoReload,
+  validateCaptcha,
+} from "react-simple-captcha";
 
 const Login = () => {
-const handleLogin = event => {
+  const captchaRef = useRef(null);
+  const [disabled, setDisabled] = useState(true);
+  const handleValidate = () => {
+    const user_captcha_value = captchaRef.current.value;
+    console.log(user_captcha_value);
+    if (validateCaptcha(user_captcha_value)) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  };
+
+  useEffect(() => {
+    loadCaptchaEnginge(6);
+  }, []);
+
+  const handleLogin = (event) => {
     event.preventDefault();
     const form = event.target;
     const email = form.email.value;
     const password = form.password.value;
     console.log(password, email);
-}
+  };
 
-    return (
-        <div>
-           <div className="hero min-h-screen bg-base-300">
-  <div className="hero-content  flex ">
-    <div className="text-center md:w-1/2 lg:text-left ">
-      <h1 className="text-5xl font-bold">Login now!</h1>
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-300">
+        <div className="hero-content  flex ">
+          <div className="text-center md:w-1/2 lg:text-left ">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+          </div>
+          <div className="card md:w-1/2 max-w-sm shadow-2xl ">
+            <form onSubmit={handleLogin} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  name="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  name="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              {/*   -------- */}
+              <div className="form-control">
+                <label className="label">
+                  <LoadCanvasTemplate />
+                </label>
+                <input
+                  ref={captchaRef}
+                  type="text"
+                  placeholder="Type here"
+                  name="type"
+                  className="input input-bordered"
+                  required
+                //   onChange={() => handleValidate(captchaRef)}
+                />
+
+                <button onClick={handleValidate}>validate</button>
+              </div>
+
+              <div className="form-control mt-6">
+                <input
+                  disabled={disabled}
+                  className="btn bg-[#D1A054B2]"
+                  type="submit"
+                  value="Login"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-    <div className="card md:w-1/2 max-w-sm shadow-2xl ">
-      <form onSubmit={handleLogin} className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="email" placeholder="email" name="email" className="input input-bordered" required />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="password" placeholder="password" name="password" className="input input-bordered" required />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-            <input className="btn bg-[#D1A054B2]" type="submit" value="Login" />
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-        </div>
-    );
+  );
 };
 
 export default Login;
